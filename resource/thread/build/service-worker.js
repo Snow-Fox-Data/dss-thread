5a6dfd439b65a696d1403165ea8cf8edc7f8d517
Ryan Moore <rmoore@excelionit.com>
2022-01-17 15:32:00 -0600

    new build

--- resource/thread/build/service-worker.js
+++ resource/thread/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "../../resource/dss-thread/build/precache-manifest.c56f211efb5d72130e8ee86f79299e56.js"
+  "../../resource/thread/build/precache-manifest.3d09e6d0a92f36a4afc8f184a730640b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("../../resource/dss-thread/build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("../../resource/thread/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
