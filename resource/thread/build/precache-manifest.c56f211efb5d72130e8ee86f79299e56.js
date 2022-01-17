15c659ebb429a48e8dea375c2b6f4f7d61ffa10c
Ryan Moore <rmoore@excelionit.com>
2022-01-17 15:19:43 -0600

    rename

---
+++ resource/thread/build/precache-manifest.c56f211efb5d72130e8ee86f79299e56.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37c9398206389db2a26c9ea54f258a0b",
+    "url": "../../resource/dss-thread/build/index.html"
+  },
+  {
+    "revision": "c5602a224caac6a6cbb4",
+    "url": "../../resource/dss-thread/build/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "a9cb7ec8ec648d4a5713",
+    "url": "../../resource/dss-thread/build/static/css/main.4ef52917.chunk.css"
+  },
+  {
+    "revision": "c5602a224caac6a6cbb4",
+    "url": "../../resource/dss-thread/build/static/js/2.c17ce16e.chunk.js"
+  },
+  {
+    "revision": "cf7a80113de6d5cafd76f2d791619f2c",
+    "url": "../../resource/dss-thread/build/static/js/2.c17ce16e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9cb7ec8ec648d4a5713",
+    "url": "../../resource/dss-thread/build/static/js/main.039dfd2b.chunk.js"
+  },
+  {
+    "revision": "4ef41d0261288a619b9a",
+    "url": "../../resource/dss-thread/build/static/js/runtime-main.7f7615b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "../../resource/dss-thread/build/static/media/logo.5d5d9eef.svg"
+  }
+]);
