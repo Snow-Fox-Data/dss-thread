4fbd8ed6d447e57f58080f8f9cf05420b865974e
Ryan Moore <rmoore@excelionit.com>
2022-02-02 09:07:48 -0600

    Update backend.py

--- webapps/thread/backend.py
+++ webapps/thread/backend.py
@@ -340,23 +340,18 @@ def get_ds_lineage(all_projects):
                 print(e)
 
     # get the full dataset lineage
-    for p in all_projects:
-        project = all_projects[p]
-        for d in range(len(project['datasets'])):
-            ds = project['datasets'][d]
-            ds['full_name'] = get_full_dataset_name(ds['name'], p)
+    # for p in all_projects:
+    #     project = all_projects[p]
+    #     for d in range(len(project['datasets'])):
+    #         ds = project['datasets'][d]
+    #         ds['full_name'] = get_full_dataset_name(ds['name'], p)
 
-            if 'lineage_upstream' in ds:
-                traverse_lineage(ds['full_name'], all_projects, upstream=True)
+    #         if 'lineage_upstream' in ds:
+    #             traverse_lineage(ds['full_name'], all_projects, upstream=True)
 
-            if 'lineage_downstream' in ds:
-                traverse_lineage(ds['full_name'], all_projects, upstream=False)
+    #         if 'lineage_downstream' in ds:
+    #             traverse_lineage(ds['full_name'], all_projects, upstream=False)
                
-            # for i in range(len(ds['schema']['columns'])):
-            #     col = ds['schema']['columns'][i]
-            #     up, down = get_col_lineage(ds, col['name'], all_projects)
-            #     col['lineage_upstream'] = up
-            #     col['lineage_downstream'] = down
              
 
 def traverse_lineage(ds_name, all_projects, upstream=True):
