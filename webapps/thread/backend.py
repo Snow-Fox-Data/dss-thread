534e5bf3cdeb6cd6e76a8451099dd0f4938d605f
Ryan Moore <rmoore@excelionit.com>
2022-02-10 15:33:29 -0600

    Update backend.py

--- webapps/thread/backend.py
+++ webapps/thread/backend.py
@@ -336,6 +336,9 @@ def get_ds_lineage(all_projects):
                         # print(recipe)
                     try:
                         ds = get_ds_by_name(i, all_projects, p)
+                        if recipe['name'] != 'split_churn_prepared':
+                            print(ds)
+                            print('-------')
                         if ds is not None:
                             if not 'lineage_downstream' in ds:
                                 ds['lineage_downstream'] = outs
@@ -351,16 +354,13 @@ def get_ds_lineage(all_projects):
                         # print(recipe)
                     try:
                         ds = get_ds_by_name(o, all_projects, p)
-                        if recipe['name'] != 'split_churn_prepared':
-                            print(ds)
-                            print('-------')
-                        # if ds is not None:
-                        #     if not 'lineage_upstream' in ds:
-                        #         ds['lineage_upstream'] = ins
-                        #     else:
-                        #         for i in ins:
-                        #             if not i in ds['lineage_upstream']:
-                        #                 ds['lineage_upstream'].append(i)
+                        if ds is not None:
+                            if not 'lineage_upstream' in ds:
+                                ds['lineage_upstream'] = ins
+                            else:
+                                for i in ins:
+                                    if not i in ds['lineage_upstream']:
+                                        ds['lineage_upstream'].append(i)
 
                     except Exception as e: 
                         capture_exception(e)
