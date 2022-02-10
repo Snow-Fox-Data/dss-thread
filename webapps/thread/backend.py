0b7eb9ced999dcadfad8c19d20c1f61251da8309
Ryan Moore <rmoore@excelionit.com>
2022-02-10 16:09:08 -0600

    Update backend.py

--- webapps/thread/backend.py
+++ webapps/thread/backend.py
@@ -193,7 +193,7 @@ def scan_server(ds_ds):
     get_ds_lineage2(scan_obj)
     print('end get lineage')
 
-    print(json.dumps(scan_obj))
+    # print(json.dumps(scan_obj))
 
     for p in scan_obj:
         datasets = scan_obj[p]['datasets']
