b7e41cdac4110733dbcccfca089f064aa5d41227
Ryan Moore <rymoore77@gmail.com>
2022-03-02 04:50:32 -0600

    no message

--- webapps/thread/backend.py
+++ webapps/thread/backend.py
@@ -268,10 +268,8 @@ class dss_utils:
 
         if col_lineage:
             for col in schema:
-                up_lin = self.get_col_lineage(col['name'], lin_up, True)
-                # print(up_lin)
-                col['lineage_upstream'] = json.dumps(up_lin)
-                # col['lineage_downstream'] = self.get_col_lineage(col['name'], lin_down, False)
+                col['lineage_upstream'] = self.get_col_lineage(col['name'], lin_up, True)
+                col['lineage_downstream'] = self.get_col_lineage(col['name'], lin_down, False)
 
         res = {
             "schema": schema,
