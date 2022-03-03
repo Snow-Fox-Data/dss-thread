3475ee521de19d3ba0dddb8f89fb3e34e4cdd1da
Ryan Moore <rymoore77@gmail.com>
2022-03-03 15:05:16 -0600

    no message

--- webapps/thread/backend.py
+++ webapps/thread/backend.py
@@ -48,7 +48,7 @@ def search():
     idx_ds = dss.get_index_ds()
     df = idx_ds.get_dataframe()
 
-    result = df[df['name'].str.contains(args.get('term'))]
+    result = df[df['name'].str.contains(args.get('term'), case=False)]
 
     return result.to_json(orient="records")
 
