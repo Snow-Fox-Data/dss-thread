dda570b00652c6acaae760fcc64ee9b0c9cca05e
Ryan Moore <rymoore77@gmail.com>
2022-03-09 15:18:57 -0600

    no message

--- webapps/thread/backend.py
+++ webapps/thread/backend.py
@@ -97,9 +97,9 @@ def update_desc():
     if exists:
         df = desc_ds.get_dataframe()
     
-    # data = request.values
-    # desc_id = int(data['id'])
-    print(request.json)
+    data = request.json
+    desc_id = int(data['id'])
+    print(desc_id)
     # applied_to = json.loads(data['applied_to'])
 
     # # print(desc_id, exists)
