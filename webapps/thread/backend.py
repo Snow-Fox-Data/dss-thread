f8460fd243dc34d09aa9294eb1f5b8ceeaff2dc1
Ryan Moore <rymoore77@gmail.com>
2022-03-09 15:27:34 -0600

    no message

--- webapps/thread/backend.py
+++ webapps/thread/backend.py
@@ -89,7 +89,7 @@ def load_item():
                 col['definition'] = {}
 
                 if len(result) > 0:
-                    col['definition'] = json.dumps(result.iloc[0])
+                    col['definition'] = result.iloc[0].to_json()
 
                 return col
 
