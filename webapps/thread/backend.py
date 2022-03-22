016cedc34a7b2ae40d5771e72e433bb2e8712956
Ryan Moore <rymoore77@gmail.com>
2022-03-21 21:38:27 -0500

    no message

--- webapps/thread/backend.py
+++ webapps/thread/backend.py
@@ -64,7 +64,7 @@ def search():
     df = idx_ds.get_dataframe()
 
     result = df[df['name'].str.contains(args.get('term'), case=False)]
-    df['key'] = df['key'].str.replace('|', ' | ')
+    result['key'] = result['key'].str.replace('|', ' | ')
 
     return result.to_json(orient="records")
 
