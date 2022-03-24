b581c90317523f5cd09c9a05ea2c188ed10ac8ff
Clifford Duda <clifford@milkcan.io>
2022-03-24 12:27:25 -0500

    Removed observer.

---
+++ resource/thread/build/precache-manifest.a5740d4012868460c9ab10023de32684.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dbc505a7ce684cda1eea097a4aa5b24",
+    "url": "../../resource/thread/build/index.html"
+  },
+  {
+    "revision": "5948f6c4843993a97a4f",
+    "url": "../../resource/thread/build/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "6d167bd8a3cedba42a32",
+    "url": "../../resource/thread/build/static/css/main.438fd272.chunk.css"
+  },
+  {
+    "revision": "5948f6c4843993a97a4f",
+    "url": "../../resource/thread/build/static/js/2.1afad273.chunk.js"
+  },
+  {
+    "revision": "cf7a80113de6d5cafd76f2d791619f2c",
+    "url": "../../resource/thread/build/static/js/2.1afad273.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d167bd8a3cedba42a32",
+    "url": "../../resource/thread/build/static/js/main.ba5923e7.chunk.js"
+  },
+  {
+    "revision": "fc5f733082d4d709244f",
+    "url": "../../resource/thread/build/static/js/runtime-main.8e4d5060.js"
+  }
+]);
