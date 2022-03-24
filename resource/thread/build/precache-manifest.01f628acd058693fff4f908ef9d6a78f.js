d1777637e4051b806677da5a31c5802ae274d181
Ryan Moore <rymoore77@gmail.com>
2022-03-23 19:23:53 -0500

    no message

---
+++ resource/thread/build/precache-manifest.01f628acd058693fff4f908ef9d6a78f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46cc67bee2d097e7d09fc9ba522d7d28",
+    "url": "../../resource/thread/build/index.html"
+  },
+  {
+    "revision": "0c1f483ba9c251575306",
+    "url": "../../resource/thread/build/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "7bfcf60f1f53d4f2a79b",
+    "url": "../../resource/thread/build/static/css/main.b8586d57.chunk.css"
+  },
+  {
+    "revision": "0c1f483ba9c251575306",
+    "url": "../../resource/thread/build/static/js/2.8c4daf7d.chunk.js"
+  },
+  {
+    "revision": "cf7a80113de6d5cafd76f2d791619f2c",
+    "url": "../../resource/thread/build/static/js/2.8c4daf7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bfcf60f1f53d4f2a79b",
+    "url": "../../resource/thread/build/static/js/main.53e2b985.chunk.js"
+  },
+  {
+    "revision": "fc5f733082d4d709244f",
+    "url": "../../resource/thread/build/static/js/runtime-main.8e4d5060.js"
+  }
+]);
--- resource/thread/build/service-worker.js
+++ resource/thread/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "../../resource/thread/build/precache-manifest.016e54bd61b3d960576ca789c0ba1ed6.js"
+  "../../resource/thread/build/precache-manifest.01f628acd058693fff4f908ef9d6a78f.js"
 );
 
 self.addEventListener('message', (event) => {
