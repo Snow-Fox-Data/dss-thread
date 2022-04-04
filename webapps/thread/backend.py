a04aa1a84703dc846ffea44115b84bbdcdc216e6
Ryan Moore <rymoore77@gmail.com>
2022-04-04 06:05:52 -0500

    no message

--- webapps/thread/backend.py
+++ webapps/thread/backend.py
@@ -629,13 +629,14 @@ class dss_utils:
                     ds_list.append(obj)
 
         # add definitions to index
-        # df = dataiku.Dataset(THREAD_DEFINITIONS_NAME).get_dataframe()
-        # for idx, row in df.iterrows():
-        #     index_list.append({
-        #             "name": row['name'],
-        #             "object_type": "definition",
-        #             "key": row['id']
-        #         })
+        df = dataiku.Dataset(THREAD_DEFINITIONS_NAME).get_dataframe()
+        for idx, row in df.iterrows():
+            index_list.append({
+                    "name": row['name'],
+                    "object_type": "definition",
+                    "key": row['id'],
+                    "description": row['name'] + ' | ' + row['description']
+                })
 
         # datasets dataset
         df = pd.DataFrame.from_dict(ds_list)
