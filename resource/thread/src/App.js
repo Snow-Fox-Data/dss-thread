fc9e9a9549bb37b062e1039db3173b4d41a0a5a6
Clifford Duda <clifford@milkcan.io>
2022-04-08 19:47:05 -0500

    Trying another routes setup.

--- resource/thread/src/App.js
+++ resource/thread/src/App.js
@@ -163,10 +163,15 @@ class App extends Component {
                             <Route path={App.CURRENT_URL + "/catalog"} element={<Catalog />} />
                         </Routes> */}
 
-                        <Routes>
+                        <Route path="/" element={<App />}>
+                            <Route index element={<Home />} />
+                            <Route path="/catalog" element={<Catalog />} />
+                        </Route>
+
+                        {/* <Routes>
                             <Route path="/" element={<Home />} />
                             <Route path="/catalog" element={<Catalog />} />
-                        </Routes>
+                        </Routes> */}
 
                         {/* <Switch>
                             <Route path="/" element={<Home />} />
