f5303a046e5eb8e740812ec011f1af8526f8b08a
Ryan Moore <rymoore77@gmail.com>
2022-04-11 13:17:21 -0500

    no message

--- resource/thread/src/pages/home.js
+++ resource/thread/src/pages/home.js
@@ -272,9 +272,9 @@ class Home extends Component {
     collectionStats() {
         return <div style={{padding:'20px'}}>
             <h1>Dataiku Instance Stats</h1>
-            <h2><span style={{color:'red'}}>{this.state.collectionStats.project_ct}</span> Projects</h2>
-            <h2><span style={{color:'red'}}>{this.state.collectionStats.dataset_ct}</span> Datasets</h2>
-            <h2><span style={{color:'red'}}>{this.state.collectionStats.column_ct}</span> Columns</h2>
+            <h2><span style={{color:'red'}}>{this.state.collectionStats.project_ct.toLocaleString("en-US")}</span> Projects</h2>
+            <h2><span style={{color:'red'}}>{this.state.collectionStats.dataset_ct.toLocaleString("en-US")}</span> Datasets</h2>
+            <h2><span style={{color:'red'}}>{this.state.collectionStats.column_ct.toLocaleString("en-US")}</span> Columns</h2>
         </div>
     }
 
