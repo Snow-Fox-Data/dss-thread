80d9ebb87e53158a8f062fb9e21f2c800a3ab11d
Ryan Moore <rymoore77@gmail.com>
2022-04-13 18:52:36 -0500

    no message

--- webapps/thread/backend.py
+++ webapps/thread/backend.py
@@ -648,6 +648,7 @@ class dss_utils:
 
                 for l in ds['lineage_upstream']:
                     if not p in l:
+                        print(p, l)
                         # this is a reference to a share
                         shared_dataset = self.get_ds_by_name(l, all_projects)
                         shared_dataset['lineage_downstream'].append(ds['full_name'])
