f345f5b8838dc66a3e04173c683ee5556b6588d5
Ryan Moore <rymoore77@gmail.com>
2022-04-20 19:01:04 -0500

    no message

---
+++ resource/thread/build/precache-manifest.8c0c8aa6d6669d43ed5e3a49e1e102ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21f72c70576c9011b75079bdf9272231",
+    "url": "../../resource/thread/build/index.html"
+  },
+  {
+    "revision": "b14469ad3cce0397705a",
+    "url": "../../resource/thread/build/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "03bc0d8ffca81c25a8c0",
+    "url": "../../resource/thread/build/static/css/main.b0890950.chunk.css"
+  },
+  {
+    "revision": "b14469ad3cce0397705a",
+    "url": "../../resource/thread/build/static/js/2.4de7250d.chunk.js"
+  },
+  {
+    "revision": "2a04dcd686c57151071e804a79ca1c19",
+    "url": "../../resource/thread/build/static/js/2.4de7250d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03bc0d8ffca81c25a8c0",
+    "url": "../../resource/thread/build/static/js/main.500db577.chunk.js"
+  },
+  {
+    "revision": "fc5f733082d4d709244f",
+    "url": "../../resource/thread/build/static/js/runtime-main.8e4d5060.js"
+  },
+  {
+    "revision": "9fac5d7cb7a3f9c8208bb3b6bd2fa436",
+    "url": "../../resource/thread/build/static/media/thread-spinner.9fac5d7c.gif"
+  }
+]);
--- resource/thread/src/App.js
+++ resource/thread/src/App.js
@@ -15,8 +15,6 @@ import {
 } from 'react-bootstrap';
 
 import {
-    BrowserRouter as Router,
-    Switch,
     Route,
     Link,
     Routes
@@ -182,7 +180,7 @@ class App extends Component {
                                     <li class="nav-item">
                                         <Link className={activeTab == App.HOME ? 'active' : ''}
                                             onClick={() => this.setState({ activeTab: App.HOME })}
-                                            to="/home">Home</Link>
+                                            to="/">Home</Link>
                                     </li>
                                     <li class="nav-item">
                                         <Link className={activeTab == App.CATALOG ? 'active' : ''}
@@ -202,8 +200,8 @@ class App extends Component {
                         <Row>
                             <Routes>
                                 {/* STANDARD ROUTES */}
-                                    <Route exact path="/home" element={<Home />} />
-                                    <Route exact path="/catalog" element={<Catalog />} />
+                                    <Route path="/" caseSensitive={false}  element={<Home />} />
+                                    <Route path="/catalog" caseSensitive={false} element={<Catalog />} />
                                 {/* <Route exact path={App.CURRENT_URL} element={<Home />} />
                                 <Route exact path={App.CURRENT_URL + "/catalog"} element={<Catalog />} /> */}
                             </Routes>
