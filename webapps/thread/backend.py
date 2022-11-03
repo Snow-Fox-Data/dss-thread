5ee70393e6c0282ba183cc97690e4dc54e81caaa
Ryan Moore <rymoore77@gmail.com>
2022-11-03 05:44:01 -0500

    rate limiting python profiling

--- webapps/thread/backend.py
+++ webapps/thread/backend.py
@@ -37,7 +37,7 @@ sentry_sdk.init(
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     # We recommend adjusting this value in production.
-    traces_sample_rate=1.0
+    traces_sample_rate=0.01
 )
 
 THREAD_CURRENT_VERSION = '1.1.4'
